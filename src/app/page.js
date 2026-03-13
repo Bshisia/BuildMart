b4b88749973bd8cc97afb9a4e1b1473b8d841d98
bshisia <shisiabrian7@gmail.com>
2026-03-13 14:11:08 +0300

    Add dark mode background to home page

--- src/app/page.js
+++ src/app/page.js
@@ -57,7 +57,7 @@ export default function Home() {
   };
 
   return (
-    <div className="min-h-screen bg-gray-50">
+    <div className="min-h-screen bg-gray-50 dark:bg-gray-900 transition-colors">
       <Header 
         cart={cart} 
         onCartClick={() => setIsCartOpen(true)} 
